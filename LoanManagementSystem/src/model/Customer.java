7e7aeae4c398327fe026e704014509273ae12e73
NEGASHREE <negashree2203@gmail.com>
2026-02-24 12:01:48 +0530

    added loan and user class basic implementation

--- LoanManagementSystem/src/model/Customer.java
+++ LoanManagementSystem/src/model/Customer.java
@@ -1,5 +1,45 @@
 package LoanManagementSystem.src.model;
 
-public class Customer {
+import java.util.List;
+import java.util.ArrayList;
+
+public class Customer extends User {
+    private String phonenumber; //private bcause customer id only needs phone number
+    private int creditScore;
+    private List<Loan> loans; //one customer can have multiple loans
+
+    public Customer(int userId, String name, String phonenumber, int creditScore) {
+        super(userId, name);
+        this.phonenumber = phonenumber;
+        this.creditScore = creditScore;
+        this.loans = new ArrayList<>(); // prevents nullpointerexception
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void applyLoan(Loan loan)
+    {
+        loans.add(loan);
+    }
+
+    public void viewLoans()
+    {
+        for(Loan loan:loans)
+        {
+            Loan.viewSummary(loan);//this will be in Loan class
+            
+        }
+    }
     
+    public void payEmi(int loanId,double amount)
+    {
+        //implemented later
+    }
+
+    public void payPrincipal(int loanId,double amount)
+    {
+        //implemented later
+    }
 }
--- LoanManagementSystem/src/model/User.java
+++ LoanManagementSystem/src/model/User.java
@@ -1,5 +1,22 @@
 package LoanManagementSystem.src.model;
 
-public class User {
-    
+public abstract class User {
+    protected int  userId; //protected bcause only the class that inherits need this properties(Encapsulation)
+    protected String name;
+
+    public User(int userId,String name) //child class in their constructor must call super(int id,string name)
+    {
+        this.userId=userId;
+        this.name=name;
+    }
+
+    public void getUserId()
+    {
+        return userId;
+    }
+
+    public void getName()
+    {
+        return name;
+    }
 }
