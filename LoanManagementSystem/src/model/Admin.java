27bc46268de4541b034c61c3d0be94802c1cdb8e
NEGASHREE <negashree2203@gmail.com>
2026-02-24 12:13:05 +0530

    added admin class  basic implementation

--- LoanManagementSystem/src/model/Admin.java
+++ LoanManagementSystem/src/model/Admin.java
@@ -1,5 +1,31 @@
 package LoanManagementSystem.src.model;
 
-public class Admin {
-    
+public class Admin extends User {
+    private String role;
+
+    public Admin(int userId,String name,String role)
+    {
+        super(userId,name);
+        this.role=role;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+
+    public void approveLoan(Loan loan)
+    {
+        loan.approve();
+    }
+
+    public void rejectLoan(Loan loan)
+    {
+        loan.reject();
+    }
+
+    public void viewAllLoans()
+    {
+        //implenetied via service/repository
+    }
 }
